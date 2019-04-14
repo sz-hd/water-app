b0b1ef428c792fe4868227f79dac30c5a236d0ae
wken01 <yuanxuemin-398@163.com>
2019-04-14 21:37:00 +0800

    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,7 @@ const app = getApp()
 Page({
   data: {
     imgUrls: [
-'http://gw.alicdn.com/imgextra/i4/195/TB2TTzyaCiJ.eBjSszfXXa4bVXa_!!195-0-yamato.jpg_q50.jpg',
-      'http://gw.alicdn.com/imgextra/i2/196/TB2XHXZamCI.eBjy1XbXXbUBFXa_!!196-0-yamato.jpg_q50.jpg'
+'http://gw.alicdn.com/imgextra/i4/195/TB2TTzyaCiJ.eBjSszfXXa4bVXa_!!195-0-yamato.jpg_q50.jpg'
     ],
     indicatorDots: true,
     autoplay: false,
