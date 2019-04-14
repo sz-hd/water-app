8ec285be1e6ea0563ecb4e7b4c365adf8c031a14
wken01 <yuanxuemin-398@163.com>
2019-04-15 00:22:17 +0800

    提交

---
+++ pages/my/my.js
@@ -0,0 +1,60 @@
+var util = require('../../utils/util.js')
+
+const app = getApp()
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          }, function () {
+            console.log('callback')
+          })
+        }
+      })
+    }
+  },
+  onPullDownRefresh: function (options) {
+    console.log('onPullDownRefresh')
+  },
+  onShareAppMessage: function (options) {
+    console.log('onShareAppMessage')
+  },
+  onReachBottom: function (options) {
+    console.log('onReachBottom')
+  },
+  onPageScroll: function (options) {
+    console.log('onPageScroll')
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ pages/order/order.js
@@ -0,0 +1,27 @@
+const util = require('../../utils/util.js')
+//https://www.cnblogs.com/DurantSimpson/p/9509824.html --分页
+Page({
+  data: {
+    tabs: ["全部","配送中", "已付款"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  }
+})
